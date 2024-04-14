f752ba1a35dfe1bae16211cbff8558ac4d01cfca
AliMuavia7178 <ghaznavghaznav@gmail.com>
2024-04-14 23:02:52 +0500

    first committo-do-list project to github

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let conditon = true;
+while (conditon) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What you want to add in your Todo?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false"
+        }
+    ]);
+    todos.push(addTask.todo);
+    conditon = addTask.addMore;
+    console.log(todos);
+}
