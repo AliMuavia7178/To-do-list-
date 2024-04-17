1398eb236a0d96dc2c351a81dd2aee2c68fedf85
AliMuavia7178 <ghaznavghaznav@gmail.com>
2024-04-17 23:43:38 +0500

    taste

--- index.ts
+++ index.ts
@@ -1,26 +1,24 @@
 #! /usr/bin/env node
-import inquirer from"inquirer"
+import inquirer from "inquirer";
 let todos = [];
 
-let conditon =true;
-while (conditon)
+let conditon = true;
+while (conditon) {
+  let addTask = await inquirer.prompt([
+    {
+      name: "todo",
+      type: "input",
+      message: "What you want to add in your Todo?",
+    },
 
-{let addTask = await inquirer.prompt(
-    [
-        {
-            name:"todo",
-            type:"input",
-            message:"What you want to add in your Todo?"
-
-},
-{
-    name:"addMore",
-    type:"confirm",
-    message:"Do you want to add more?",
-    default:"false"
+    {
+      name: "addMore",
+      type: "confirm",
+      message: "Do you want to add more?",
+     default: "false"
+    },
+  ]);
+  todos.push(addTask.todo);
+  conditon = addTask.addMore;
+  console.log(todos);
 }
-]
-);
-todos.push(addTask.todo);
-conditon = addTask.addMore
-console.log(todos)}
